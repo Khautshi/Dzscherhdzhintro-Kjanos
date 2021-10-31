ec73ca630b056a4d7e5aeff3b7a04e5b5cf5926b
Khautshi <93495746+Khautshi@users.noreply.github.com>
2021-10-31 16:03:40 -0700

    Add files via upload

---
+++ Kjanos_Interface.java
@@ -0,0 +1,12 @@
+
+public interface Kjanos_Interface {
+
+    public void set_Tscher();
+
+    public void set_NJSR();
+
+    public void set_Bengmellanbli();
+
+    public String get_Kjanos();
+
+}
